f403f9f3d836c7621a1a6aece99c630411def67a
Alina Kulmagambetova <playpurplepiano@outlook.com>
2017-10-10 18:40:07 +0300

    по второму клику на меню показываются все посты, магия вне хогвартса!

--- проба скрипты.js
+++ проба скрипты.js
@@ -2,43 +2,73 @@ $(document).ready( function()
 {
 	$('.menu .feminism.item').click( function() 
 	{
-		$('.post').hide();
-		$('.feminism.post').slideDown( 300 );
+		if( $(this).hasClass( 'selected' ) ) 
+		{
+			//undo the click & show all posts
+			$(this).removeClass( 'selected' );
+			$('.post').slideDown( 300 );
+		}
+		else
+		{
+			//hides unrelated posts & shows related
+			$('.post').hide();
+			$('.feminism.post').slideDown( 300 );
 
-		$('.menu .item.selected').removeClass( 'selected' );
-		$(this).addClass( 'selected' );
+			//makes menu items stay reddish when clicked
+			$('.menu .item.selected').removeClass( 'selected' );
+			$(this).addClass( 'selected' );
+		}
 	});
 
 	$('.menu .science.item').click( function()
 	{
-		//undo the click (kinda)
-		if ( true ) {}
+		if( $(this).hasClass( 'selected' ) ) 
+		{
+			$(this).removeClass( 'selected' );
+			$('.post').slideDown( 300 );
+		}
+		else
+		{
+			$('.post').hide();
+			$('.science.post').slideDown( 300 );
 
-		//hides unrelated posts & shows related
-		$('.post').hide();
-		$('.science.post').slideDown( 300 );
-
-		//makes menu items stay red when clicked
-		$('.menu .item.selected').removeClass( 'selected' );
-		$(this).addClass( 'selected' );
+			$('.menu .item.selected').removeClass( 'selected' );
+			$(this).addClass( 'selected' );
+		}
 	});
 
 	$('.menu .memes.item').click( function ()
 	{
-		$('.post').hide();
-		$('.memes.post').slideDown( 300 );
+		if( $(this).hasClass( 'selected' ) ) 
+		{
+			$(this).removeClass( 'selected' );
+			$('.post').slideDown( 300 );
+		}
+		else
+		{
+			$('.post').hide();
+			$('.memes.post').slideDown( 300 );
 
-		$('.menu .item.selected').removeClass( 'selected' );
-		$(this).addClass( 'selected' );	
+			$('.menu .item.selected').removeClass( 'selected' );
+			$(this).addClass( 'selected' );
+		}
 	});
 
 	$('.menu .personal.item').click( function()
 	{
-		$('.post').hide();
-		$('.personal.post').slideDown( 300 );
+		if( $(this).hasClass( 'selected' ) ) 
+		{
+			$(this).removeClass( 'selected' );
+			$('.post').slideDown( 300 );
+		}
+		else
+		{
+			$('.post').hide();
+			$('.personal.post').slideDown( 300 );
 
-		$('.menu .item.selected').removeClass( 'selected' );
-		$(this).addClass( 'selected' );		
+			$('.menu .item.selected').removeClass( 'selected' );
+			$(this).addClass( 'selected' );
+		}	
 	});
 
 });
