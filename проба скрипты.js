a7d089f3c4584c85499377e287dadf6eabaa8847
Aleksandr <reg@aleksandrzimin.com>
2017-10-10 18:51:39 +0300

    помог Алине рефакнуть

--- проба скрипты.js
+++ проба скрипты.js
@@ -1,74 +1,22 @@
 $(document).ready( function() 
 {
-	$('.menu .feminism.item').click( function() 
+	$('.menu .item').click( function() 
 	{
-		if( $(this).hasClass( 'selected' ) ) 
-		{
-			//undo the click & show all posts
-			$(this).removeClass( 'selected' );
-			$('.post').slideDown( 300 );
-		}
-		else
-		{
-			//hides unrelated posts & shows related
-			$('.post').hide();
-			$('.feminism.post').slideDown( 300 );
+		var $clickedMenuItem = $(this);
 
-			//makes menu items stay reddish when clicked
-			$('.menu .item.selected').removeClass( 'selected' );
-			$(this).addClass( 'selected' );
-		}
-	});
-
-	$('.menu .science.item').click( function()
-	{
-		if( $(this).hasClass( 'selected' ) ) 
+		if( $clickedMenuItem.hasClass('selected' ) )
 		{
-			$(this).removeClass( 'selected' );
+			$clickedMenuItem.removeClass('selected' );
 			$('.post').slideDown( 300 );
 		}
 		else
 		{
 			$('.post').hide();
-			$('.science.post').slideDown( 300 );
+			$('.post.' + $clickedMenuItem.data('postsClassToOpen')).slideDown( 300 );
 
 			$('.menu .item.selected').removeClass( 'selected' );
-			$(this).addClass( 'selected' );
+			$clickedMenuItem.addClass( 'selected' );
 		}
-	});
-
-	$('.menu .memes.item').click( function ()
-	{
-		if( $(this).hasClass( 'selected' ) ) 
-		{
-			$(this).removeClass( 'selected' );
-			$('.post').slideDown( 300 );
-		}
-		else
-		{
-			$('.post').hide();
-			$('.memes.post').slideDown( 300 );
-
-			$('.menu .item.selected').removeClass( 'selected' );
-			$(this).addClass( 'selected' );
-		}
-	});
-
-	$('.menu .personal.item').click( function()
-	{
-		if( $(this).hasClass( 'selected' ) ) 
-		{
-			$(this).removeClass( 'selected' );
-			$('.post').slideDown( 300 );
-		}
-		else
-		{
-			$('.post').hide();
-			$('.personal.post').slideDown( 300 );
-
-			$('.menu .item.selected').removeClass( 'selected' );
-			$(this).addClass( 'selected' );
-		}	
 	});
 
 });
